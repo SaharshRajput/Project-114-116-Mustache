78b4ab0aa3e4843fe558d5d117033fcd0a9fc638
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-04-16 12:49:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-function preload()
-{
-
+noseX="";
+noseY="";
+function preload(){
+clown_nose= loadImage("https://i.postimg.cc/mkrYs37R/mustache.png");
 }
 function setup()
 {
     canvas=createCanvas(300,300);
     canvas.center();
     video=createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
-
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on("pose",gotPoses);
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
 }
 function draw()
 {
-    image(video,0,0,300,300);
+image(video,0,0,300,300);
+image(clown_nose,noseX,noseY,70,30);
 }
 function take_snapshot()
 {
-    save("Mustache");
+    save("Realtime Filter Image");
 }
 function modelLoaded()
 {
-    console.log("PoseNet is Initialized");
+console.log("PoseNet is Initialized");
 }
 function gotPoses(results)
 {
-if(results)
-{
-    console.log(results);
-    console.log("Left eye position="+results[0].pose.leftEye.x);
-    console.log("Right eye position="+results[0].pose.rightEye.y);
-}
+    if(results.length>0)
+    {
+console.log(results);
+noseX= results[0].pose.nose.x-32;  
+noseY= results[0].pose.nose.y;
+console.log("Nose x="+noseX);
+console.log("Nose y="+noseY);
+    }
 }
